b9f3468c718880176b78f005f3df30318dd83080
biroghan <biroghlan95@gmail.com>
2021-05-03 19:09:09 +0430

    Added a authenticated and create login & registration user.

---
+++ crm1/account/views.py
@@ -0,0 +1,9 @@
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+
+
+class UserRegisterView(CreateView):
+	form_class = UserCreationForm
+	template_name = 'registration/register.html'
+	success_url = reverse_lazy('login')
