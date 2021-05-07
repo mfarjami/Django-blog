b60786e60290dfd11070e8c222ddf0e7db8e0281
biroghan <biroghlan95@gmail.com>
2021-05-07 11:02:16 +0430

    Added authenticated for each Author

--- crm1/blog/views.py
+++ crm1/blog/views.py
@@ -6,51 +6,51 @@ from .forms import *
 # Create your views here.
 
 class PostList(ListView):
-    model = Post
-    ordering = ['-publish']
+	model = Post
+	ordering = ['-publish']
 
-    def get_context_data(self, *args, **kwargs):
-        cat_menu = Category.objects.all()
-        context = super(PostList, self).get_context_data(*args, **kwargs)
-        context["cat_menu"]= cat_menu
-        return context
+	def get_context_data(self, *args, **kwargs):
+		cat_menu = Category.objects.all()
+		context = super(PostList, self).get_context_data(*args, **kwargs)
+		context["cat_menu"]= cat_menu
+		return context
 
 def CategoryListView(request):
-    cat_menu_list = Category.objects.all()
-    return render(request, 'blog/category_list.html',{'cat_menu_list':cat_menu_list})
+	cat_menu_list = Category.objects.all()
+	return render(request, 'blog/category_list.html',{'cat_menu_list':cat_menu_list})
 
 def CategoryView(request, cats):
-    category_post = Post.objects.filter(category=cats)
-    return render(request, 'blog/categories.html',{'cats':cats.title(), 'category_post':category_post})
+	category_post = Post.objects.filter(category=cats)
+	return render(request, 'blog/categories.html',{'cats':cats.title(), 'category_post':category_post})
 
 class PostDetail(DetailView):
-    model = Post
+	model = Post
 
-    def get_context_data(self, *args, **kwargs):
-        cat_menu = Category.objects.all()
-        context = super(PostDetail, self).get_context_data(*args, **kwargs)
-        context["cat_menu"]= cat_menu
-        return context
+	def get_context_data(self, *args, **kwargs):
+		cat_menu = Category.objects.all()
+		context = super(PostDetail, self).get_context_data(*args, **kwargs)
+		context["cat_menu"]= cat_menu
+		return context
 
 
 
 class PostCreate(CreateView):
-    model = Post
-    form_class = PostForm
-    # fields = '__all__'
+	model = Post
+	form_class = PostForm
+	# fields = '__all__'
 
 class PostCategoryView(CreateView):
-    model = Category
-    # form_class = UpdateForm
-    fields = '__all__'
-    # template_name = 'blog/post_update.html'
+	model = Category
+	# form_class = UpdateForm
+	fields = '__all__'
+	# template_name = 'blog/post_update.html'
 
 class PostUpdate(UpdateView):
-    model = Post
-    form_class = UpdateForm
-    # fields = ('title','body')
-    template_name = 'blog/post_update.html'
+	model = Post
+	form_class = UpdateForm
+	# fields = ('title','body')
+	template_name = 'blog/post_update.html'
 
 class PostDelete(DeleteView):
-    model = Post
-    success_url = reverse_lazy('blog:home')
+	model = Post
+	success_url = reverse_lazy('blog:home')
